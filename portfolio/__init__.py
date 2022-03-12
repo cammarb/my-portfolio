b30381fc407d5aa700e08eb45bab686c6d2f8376
Camila Martinez <camilamarb98@gmail.com>
2022-03-11 23:18:55 -0500

    Add __init__ template

---
+++ portfolio/__init__.py
@@ -0,0 +1,20 @@
+import os
+from flask import Flask
+
+
+def create_app():
+    # create and configure the app
+    app = Flask(__name__, instance_relative_config=True)
+
+    # ensure the instance folder exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+
+    # a simple page that says hello
+    @app.route('/')
+    def hello():
+        return 'Hello, World!'
+
+    return app
