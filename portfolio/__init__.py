fa190e784f45bff63f337f7208ffc6318aae671e
Camila Martinez <camilamarb98@gmail.com>
2022-03-13 18:01:04 -0500

    Add dynamic route for single projects

--- portfolio/__init__.py
+++ portfolio/__init__.py
@@ -1,6 +1,22 @@
 import os
 from flask import Flask, render_template
 
+# Test for dynamic pages
+projects = {
+    '1': {
+        "name": "Project 1",
+        "description": "This is the description for Project 1"
+    },
+    '2': {
+        "name": "Project 3",
+        "description": "This is the description for Project 2"
+    },
+    '3': {
+        "name": "Project 3",
+        "description": "This is the description for Project 3"
+    },
+}
+
 
 def create_app():
     # create and configure the app
@@ -13,15 +29,26 @@ def create_app():
         pass
 
     # Routes
-    @app.route('/')
+    @ app.route('/')
     def home():
         return render_template('home.html')
 
-    @app.route('/projects')
+    @ app.route('/projects')
     def list_projects():
         return render_template('projects.html')
 
-    @app.route('/contact')
+    @ app.route('/projects/<id>')
+    def get_project(id):
+        name = projects[id]["name"]
+        description = projects[id]["description"]
+
+        return render_template(
+            'project.html',
+            name=name,
+            description=description
+        )
+
+    @ app.route('/contact')
     def contact():
         return render_template('contact.html')
 
