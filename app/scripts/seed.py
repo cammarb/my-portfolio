58300378d5560fc8d1e16de437323d649ba69c6a
Camila Martinez <camilamarb98@gmail.com>
2022-03-20 23:22:53 -0500

    add projects

--- app/scripts/seed.py
+++ app/scripts/seed.py
@@ -1,23 +1,51 @@
 from app.app import create_app
 from app.blog.models import Blog
+from app.projects.models import Project
 from app.extensions.database import db
 
 app = create_app()
 app.app_context().push()
 
-# Test for dynamic pages
-blog_posts = {
+# Projects
+projects = {
     '1': {
-        "title": "Blog 1",
-        "content": "This is the description for Blog 1"
+        "name": "My portfolio",
+        "description": "Portfolio website made with Python & Flask",
+        "github": "https://github.com/cammarb/my-portfolio.git",
+        "image": "https://raw.githubusercontent.com/cammarb/my-portfolio/master/portfolio_screenshot.png"
     },
     '2': {
-        "title": "Blog 2",
-        "content": "This is the description for Blog 2"
+        "name": "Yugi Decks",
+        "description": "Yu-Gi-Oh library using Yu-Gi-Oh's API",
+        "github": "https://github.com/cammarb/yugi-decks.git",
+        "image": "https://raw.githubusercontent.com/cammarb/yugi-decks/master/screenshot.png"
+    }
+}
+
+for id, project in projects.items():
+    new_project = Project(
+        id=id, title=project['name'],
+        description=project['description'],
+        gh_link=project['github'],
+        image_link=project['image'])
+    db.session.add(new_project)
+
+db.session.commit()
+
+
+# Blog
+blog_posts = {
+    '1': {
+        "title": "Learning Flask development",
+        "content": "This is the content for Blog 1"
+    },
+    '2': {
+        "title": "How to install Ubuntu 22.04 LTS",
+        "content": "This is the content for Blog 2"
     },
     '3': {
-        "title": "Blog 3",
-        "content": "This is the description for Blog 3"
+        "title": "Learning GTK4 for GNOME development",
+        "content": "This is the content for Blog 3"
     },
 }
 
