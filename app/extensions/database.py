385ea978d479352247420d9602305d7c7064a305
Camila Martinez <camilamarb98@gmail.com>
2022-03-27 15:30:49 -0500

    Fix error in commit()

--- app/extensions/database.py
+++ app/extensions/database.py
@@ -8,10 +8,10 @@ migrate = Migrate()
 class CRUDMixin():
     def save(self):
         db.session.add(self)
-        db.session.commit(self)
+        db.session.commit()
         return self
 
     def delete(self):
         db.session.delete(self)
-        db.session.commit(self)
+        db.session.commit()
         return
