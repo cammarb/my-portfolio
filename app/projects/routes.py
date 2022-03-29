b7132a89207ee9aed3c1b959270d05381d07f31f
Camila Martinez <camilamarb98@gmail.com>
2022-03-29 18:03:26 -0500

    Add new projects / redirect after POST

--- app/projects/routes.py
+++ app/projects/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, redirect, render_template, request, url_for
 from .models import Project
 
 blueprint = Blueprint('projects', __name__)
@@ -6,10 +6,10 @@ blueprint = Blueprint('projects', __name__)
 # Routes
 
 
-@blueprint.route('/projects', methods=['POST', 'GET'])
+@blueprint.route('/projects')
 def projects():
     all_projects = Project.query.all()
-    return render_template('projects.html', projects=all_projects)
+    return render_template('projects/projects.html', projects=all_projects)
 
 
 @blueprint.route('/projects/<id>')
@@ -17,6 +17,24 @@ def project(id):
     project = Project.query.get(id)
 
     return render_template(
-        'project.html',
+        'projects/project.html',
+        project=project
+    )
+
+
+@blueprint.route('/projects/new', methods=('GET', 'POST'))
+def new_project():
+    if request.method == 'POST':
+        new_project = Project(
+            title=request.form['title'],
+            description=request.form['description'],
+            gh_link=request.form['gh_link'],
+            image_link=request.form['image_link'],
+        )
+        new_project.save()
+        return redirect(url_for('projects.project', id=new_project.id))
+
+    return render_template(
+        'projects/new_project.html',
         project=project
     )
