c21e5f37dfbcd5f28ab952dd6b634624cf388072
Camila Martinez <camilamarb98@gmail.com>
2022-03-30 17:20:13 -0500

    update model

--- app/users/models.py
+++ app/users/models.py
@@ -3,6 +3,6 @@ from app.extensions.database import db, CRUDMixin
 
 class User(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
     name = db.Column(db.String(100))
