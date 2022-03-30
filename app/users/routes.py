ecfc4032addd2f85e9829a24a87483d0db500086
Camila Martinez <camilamarb98@gmail.com>
2022-03-30 17:20:24 -0500

    add login route

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,11 +1,23 @@
 from .models import User
 from flask import Blueprint, render_template, request, url_for
+from werkzeug.security import generate_password_hash, check_password_hash
 
 blueprint = Blueprint('users', __name__)
 
 
 @blueprint.route('/register', methods=('GET', 'POST'))
 def register():
-    # if request.method == 'POST':
-
+    if request.method == 'POST':
+        new_user = User(
+            name=request.form['name'],
+            username=request.form['username'],
+            password=request.form['password']
+        )
+        new_user.save()
+        return render_template(url_for('users.login'))
     return render_template('users/register.html')
+
+
+# @blueprint.route('/login', methods=('GET', 'POST'))
+# def login():
+#     if request.method == 'POST':
