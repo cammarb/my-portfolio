90591bb5ba0761d991fe13da57948fbbf756349c
Camila Martinez <camilamarb98@gmail.com>
2022-03-31 19:54:19 -0500

    Change to unique 'email'

--- app/users/models.py
+++ app/users/models.py
@@ -1,8 +1,9 @@
+import email
 from app.extensions.database import db, CRUDMixin
 
 
 class User(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
     password = db.Column(db.String(100))
+    email = db.Column(db.String(100), unique=True)
     name = db.Column(db.String(100))
