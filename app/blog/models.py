60f700c186ec09969c9834a4204e83ddd7ee998a
Camila Martinez <camilamarb98@gmail.com>
2022-03-31 19:54:35 -0500

    Change to 800 words for blog post

--- app/blog/models.py
+++ app/blog/models.py
@@ -4,5 +4,5 @@ from app.extensions.database import db, CRUDMixin
 class Blog(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
-    content = db.Column(db.String(300))
+    content = db.Column(db.String(800))
     picture_url = db.Column(db.String(200))
