6f9f35442c29d4ad02161dd079915493352208a3
Camila Martinez <camilamarb98@gmail.com>
2022-04-01 17:30:20 -0500

    add flask_login to user model

--- app/users/models.py
+++ app/users/models.py
@@ -1,8 +1,8 @@
-import email
 from app.extensions.database import db, CRUDMixin
+from flask_login import UserMixin
 
 
-class User(db.Model, CRUDMixin):
+class User(db.Model, CRUDMixin, UserMixin):
     id = db.Column(db.Integer, primary_key=True)
     password = db.Column(db.String(100))
     email = db.Column(db.String(100), unique=True)
