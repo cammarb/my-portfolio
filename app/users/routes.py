6f58c3fbf789bb9f572279e046851d4c10b521c4
Camila Martinez <camilamarb98@gmail.com>
2022-04-01 17:30:31 -0500

    login_required to register

--- app/users/routes.py
+++ app/users/routes.py
@@ -1,19 +1,21 @@
 from distutils.log import error
-import email
 from msilib.schema import Error
 from .models import User
 from flask import Blueprint, redirect, render_template, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, logout_user, login_required
 
 blueprint = Blueprint('users', __name__)
 
 
 @blueprint.get('/register')
+@login_required
 def get_register():
     return render_template('users/register.html')
 
 
 @blueprint.post('/register')
+@login_required
 def post_register():
     try:
         if request.form.get('password') != request.form.get('password_confirmation'):
@@ -28,7 +30,8 @@ def post_register():
             password=generate_password_hash(request.form['password'])
         )
         new_user.save()
-        return redirect(url_for('users.login'))
+        login_user(new_user)
+        return redirect(url_for('users.get_login'))
 
     except Exception as error_message:
         error = error_message or 'An error occurred while creating a user. Please make sure to enter valid data.'
@@ -48,9 +51,16 @@ def post_login():
             raise Exception('No user with the given email address was found.')
         elif check_password_hash(request.form['password'], user.password):
             raise Exception('Incorrect password.')
-
+        login_user(user)
         return redirect(url_for('blog.blogs'))
 
     except Exception as error_message:
         error = error_message or 'An error occurred while logging in. Please verify your email and password.'
         return render_template('users/login.html', error=error)
+
+
+@blueprint.get('/logout')
+def logout():
+    logout_user()
+
+    return redirect(url_for('users.get_login'))
