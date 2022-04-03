c2a9540d8587ed4da2275b43f732e42f586ad564
Camila Martinez <camilamarb98@gmail.com>
2022-04-03 12:52:11 -0500

    update config

--- app/config.py
+++ app/config.py
@@ -3,6 +3,7 @@ from os import environ
 
 load_dotenv()
 
-SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+SQLALCHEMY_DATABASE_URI = environ.get(
+    'DATABASE_URL').replace('postgres://', 'postgresql://', 1)
 SECRET_KEY = environ.get('SECRET_KEY')
 POSTS_PER_PAGE = 4
