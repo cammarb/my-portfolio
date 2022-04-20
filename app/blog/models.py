5fd2ef4ed2f3dbfd02918f491461dfc8f6d3f9c7
Camila Martinez <camilamarb98@gmail.com>
2022-04-20 15:09:28 -0500

    add user blog many-to-many relationship

--- app/blog/models.py
+++ app/blog/models.py
@@ -1,4 +1,11 @@
+from email.policy import default
 from app.extensions.database import db, CRUDMixin
+from datetime import datetime
+
+
+class UserPost(db.Model):
+    post_id = db.Column(db.Integer, db.ForeignKey("blog.id"), primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
 
 
 class Blog(db.Model, CRUDMixin):
@@ -6,3 +13,7 @@ class Blog(db.Model, CRUDMixin):
     title = db.Column(db.String(100))
     content = db.Column(db.String(800))
     picture_url = db.Column(db.String(200))
+    date = db.Column(db.Date, nullable=False,
+                     default=datetime.date(datetime.utcnow()))
+    blog_user = db.relationship("User", secondary="user_post",
+                                backref="user_post", lazy=True)
