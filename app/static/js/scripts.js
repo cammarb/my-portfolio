f5d662bd63ec26af90713763ad06ee71e69de455
Camila Martinez <camilamarb98@gmail.com>
2022-04-21 20:13:47 -0500

    attempt adding toggle theme

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -2,18 +2,25 @@
 // the website will allways start in light mode
 // so the dark mode is disabled by default
 // Dark mode gets created if it doesn’t exist in the LocalStorage 
-if (localStorage.getItem('darkMode') === null) {
-    localStorage.setItem('darkMode', "false");
-}
+// if (localStorage.getItem('darkMode') === null) {
+//     localStorage.setItem('darkMode', "false");
+// }
 
-function activateDarkMode() {
-    var element = document.body;
-    element.classList.toggle("dark-mode");
-}
+// checkTheme()
 
+// function checkTheme() {
+//     if (localStorage.getItem('darkMode') === false) {
+//         var element = document.body;
+//         element.classList.remove("dark-mode");
+//     }
+//     else if (localStorage.getItem('darkMode') === true) {
+//         console.log(localStorage.getItem('darkMode'))
+//         var element = document.body;
+//         element.classList.add("dark-mode");
+//     }
+// }
 
 /* Hamburger Menu */
-
 window.onresize = window.onload = function () {
     width = this.innerWidth;
 
@@ -30,9 +37,6 @@ window.onresize = window.onload = function () {
     }
 }
 
-
-
-
 function toggleMenu() {
     var x = document.getElementById("menu-menu");
     if (x.className === "menu-menu-container") {
@@ -41,3 +45,28 @@ function toggleMenu() {
         x.className = "menu-menu-container";
     }
 }
+
+
+
+let theme = localStorage.getItem('data-theme');
+const changeThemeToDark = () => {
+    document.documentElement.setAttribute("data-theme", "dark") // set theme to dark
+    localStorage.setItem("data-theme", "dark") // save theme to local storage
+}
+
+const changeThemeToLight = () => {
+    document.documentElement.setAttribute("data-theme", "light") // set theme light
+    localStorage.setItem("data-theme", 'light') // save theme to local storage
+}
+
+// Get the element based on ID
+const checkbox = document.getElementById("switch");
+// Apply retrived them to the website
+checkbox.addEventListener('change', () => {
+    let theme = localStorage.getItem('data-theme'); // Retrieve saved them from local storage
+    if (theme === 'dark') {
+        changeThemeToLight()
+    } else {
+        changeThemeToDark()
+    }
+});
