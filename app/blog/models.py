80db3fc32e059cb9143806ccfb04e9b4c752bc93
Camila Martinez <camilamarb98@gmail.com>
2022-04-24 13:32:57 -0500

    remove many to many relationship

--- app/blog/models.py
+++ app/blog/models.py
@@ -3,11 +3,6 @@ from app.extensions.database import db, CRUDMixin
 from datetime import datetime
 
 
-class UserPost(db.Model):
-    post_id = db.Column(db.Integer, db.ForeignKey("blog.id"), primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), primary_key=True)
-
-
 class Blog(db.Model, CRUDMixin):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(100))
@@ -15,5 +10,4 @@ class Blog(db.Model, CRUDMixin):
     picture_url = db.Column(db.String(200))
     date = db.Column(db.Date, nullable=False,
                      default=datetime.date(datetime.utcnow()))
-    blog_user = db.relationship("User", secondary="user_post",
-                                backref="user_post", lazy=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
