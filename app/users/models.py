cbebaf83c4ee5c318802f28f1ac6e5ff3eab1bfe
Camila Martinez <camilamarb98@gmail.com>
2022-04-24 13:32:46 -0500

    fix realtionship many to one

--- app/users/models.py
+++ app/users/models.py
@@ -7,3 +7,4 @@ class User(db.Model, CRUDMixin, UserMixin):
     email = db.Column(db.String(100), unique=True)
     name = db.Column(db.String(100))
     passwd = db.Column(db.String(300))
+    blogs = db.relationship("Blog", backref="user", lazy=True)
