13a505ec59e53fd76b61aeb029e3a5e8444d89ae
Camila Martinez <camilamarb98@gmail.com>
2022-04-28 16:37:37 -0500

    fix dark mode js

--- app/static/js/darkMode.js
+++ app/static/js/darkMode.js
@@ -3,15 +3,14 @@ let darkMode = localStorage.getItem('darkMode');
 const switchBtn = document.querySelector("#switch");
 const themeSwitcher = document.querySelector("#theme-switcher");
 
-
 const enableDarkMode = () => {
-    document.body.classList.add('dark-mode');
+    document.documentElement.classList.add('dark-mode');
     localStorage.setItem('darkMode', 'enabled');
     switchBtn.classList.add("dark-mode-active");
 }
 
 const disableDarkMode = () => {
-    document.body.classList.remove('dark-mode');
+    document.documentElement.classList.remove('dark-mode');
     localStorage.setItem('darkMode', null);
     switchBtn.classList.remove("dark-mode-active");
 }
