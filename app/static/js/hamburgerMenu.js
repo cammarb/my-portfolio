0159cd5ebdb81a3fb6abbe95359e3909fda37d04
Camila Martinez <camilamarb98@gmail.com>
2022-04-27 19:21:02 -0500

    toggle hamburguer menu

--- app/static/js/hamburgerMenu.js
+++ app/static/js/hamburgerMenu.js
@@ -1,7 +1,13 @@
-const hamburgerBtn = document.getElementById("hamburger-icon")
-let menu = document.getElementById("primary-navigation");
+const primaryNav = document.querySelector(".primary-navigation");
+const hamburgerBtn = document.querySelector("#hamburger-button");
 
-// TODO: Responsive hamburger menu
-hamburgerBtn.onclick = () => {
-    menu.classList.toggle("show");
-};
+hamburgerBtn.addEventListener('click', () => {
+    const visibility = primaryNav.getAttribute("data-visible");
+
+    if (visibility === "false") {
+        primaryNav.setAttribute("data-visible", true);
+    }
+    else if (visibility === "true") {
+        primaryNav.setAttribute("data-visible", false);
+    }
+});
