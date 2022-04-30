e01f52b42b6e450d7a7dc09f313a649e8170624d
Camila Martinez <camilamarb98@gmail.com>
2022-04-30 16:15:44 -0500

    refactor routes

--- app/users/routes.py
+++ app/users/routes.py
@@ -2,6 +2,7 @@ from .models import User
 from flask import Blueprint, redirect, render_template, request, url_for
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import login_user, logout_user, login_required
+from .services import *
 
 blueprint = Blueprint('users', __name__)
 
@@ -16,19 +17,7 @@ def get_register():
 # @login_required
 def post_register():
     try:
-        if request.form.get('password') != request.form.get('password_confirmation'):
-            raise Exception(
-                'The password confirmation must match the password.')
-        elif User.query.filter_by(email=request.form.get('email')).first():
-            raise Exception('The email address is already registered.')
-
-        new_user = User(
-            name=request.form['name'],
-            email=request.form['email'],
-            passwd=generate_password_hash(request.form['password'])
-        )
-        new_user.save()
-        login_user(new_user)
+        register(request.form)
         return redirect(url_for('users.get_login'))
 
     except Exception as error_message:
@@ -44,12 +33,7 @@ def get_login():
 @blueprint.post('/login')
 def post_login():
     try:
-        user = User.query.filter_by(email=request.form['email']).first()
-        if not user:
-            raise Exception('No user with the given email address was found.')
-        elif check_password_hash(request.form['password'], user.passwd):
-            raise Exception('Incorrect password.')
-        login_user(user)
+        login(request.form)
         return redirect(url_for('blog.blogs'))
 
     except Exception as error_message:
