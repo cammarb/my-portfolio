770cc649a1c95ac91e4df03caa6f0ba1eb9433e8
Camila Martinez <camilamarb98@gmail.com>
2022-05-01 18:31:52 -0500

    add tests

--- app/tests/blog/test_routes.py
+++ app/tests/blog/test_routes.py
@@ -1,4 +1,5 @@
 from app.blog.models import Blog
+from app.users.models import User
 from datetime import datetime
 
 
@@ -7,9 +8,18 @@ def test_blog_renders_blog(client):
     new_post = Blog(title='Test Blog',
                     content='This is a test',
                     picture_url='https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/Flask_logo.svg/1200px-Flask_logo.svg.png',
-                    date=datetime.date(datetime.utcnow()))
+                    date=datetime.date(datetime.utcnow()),
+                    user_id=1)
     new_post.save()
 
     response = client.get('/blogs')
 
     assert b'Test Blog' in response.data
+
+
+# def test_post_user_blog(client):
+#     # Creates a blog related to the user
+#     response = client.post('/blogs/new', data={
+#         "id": 1,
+#     })
+#     assert User.query.get(1) is 1
